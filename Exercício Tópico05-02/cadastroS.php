4fe75f0037917cc2c9a8b35ab110e8b5965be4da
pemaldonadot <peepe_maldonado@hotmail.com>
2017-11-01 14:34:04 -0200

    Ajustar detalhes - SORT

--- Exercício Tópico05-02/cadastroS.php
+++ Exercício Tópico05-02/cadastroS.php
@@ -1,45 +1,51 @@
 <?php
-// Inicia uma session para que seja possível utilizar o array aluno em outras páginas 
-	session_start();
-	$_SESSION['cadastro'] = $aluno;
+        session_start();
+        $aluno =  array();
+        $_SESSION['cadastro'] = $aluno;
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Contador</title>
-        <link rel="stylesheet" type="text/css" href="css.css"/>
+        <title>form</title>
     </head>
     <body>
 
     <div id="menu">
         <a href="lista.php">Ver Lista</a>
-        <a href="cadastro.php">Ver Cadastro</a>
     </div>
-  				<!-- Ele retorna a própria página porque ele irá ler a função cadastrar.php -->
-            <center><form action="cadastro.php"method="POST">
-                <input type="text" name="nome"     placeholder="Nome Completo"></br>
-                <input type="number" name="ra"     placeholder="RA"></br>
-                <select name="gender">
-                    <option value="Masculino">Masculino
-                    <option value="Feminino">Feminino
-                    <option value="Outro">Outro
-                </select>
-                </br>
-                <input type="number" name="idade"  min="1" max="99"  placeholder="Idade"></br>
-                <input type="text" name="endereco" placeholder="Endereço"></br>
-                <input type="tel" name="telefone" placeholder="Telefone"></br>
-                <input type="email" name="email"    placeholder="email"></br></br>
-                <input type="button" name="cadastrar" onclick="cadastrar()" value="Cadastrar">
-                <input input type="submit" value="Exibir" name="submit">
-            </form></center>
-            <?php
-				$aluno = []; 
-            function cadastrar(){
-        			array_push($aluno, $_POST["nome"], $_POST["ra"], $_POST["gender"], 
-				$_POST["idade"], $_POST["endereco"], $_POST["telefone"], $_POST["email"]);
+            <center>
+                <form action="cadastroS.php"method="POST">
+                    <input type="text" name="nome"     placeholder="Nome Completo"></br>
+                    <input type="number" name="ra"     placeholder="RA"></br>
+                    <select name="gender">
+                        <option value="Masculino">Masculino
+                        <option value="Feminino">Feminino
+                        <option value="Outro">Outro
+                    </select>
+                    </br>
+                    <input type="number" name="idade"  min="1" max="99"  placeholder="Idade"></br>
+                    <input type="text" name="endereco" placeholder="Endereço"></br>
+                    <input type="tel" name="telefone" placeholder="Telefone"></br>
+                    <input type="email" name="email"    placeholder="email"></br></br>
+                    <input type="submit" name="cadastrar" value="Cadastrar">
+                </form>
+            </center>
+        
+        <?php
+
+            var_dump($_REQUEST);
+            var_dump($_SESSION['cadastro']);
+            if(isset($_SESSION['cadastro'])){
+            array_push($_SESSION['cadastro'],   $_REQUEST['nome'],
+                                                $_REQUEST["ra"],
+                                                $_REQUEST["gender"],
+                                                $_REQUEST["idade"],
+                                                $_REQUEST["endereco"],
+                                                $_REQUEST["telefone"],
+                                                $_REQUEST["email"]);
             }
-            ?>
+        ?>
 
 
     </body>
--- Exercício Tópico05-02/lista.php
+++ Exercício Tópico05-02/lista.php
@@ -1,20 +1,7 @@
 <?php
 	// Recebe as variaveis do tipo Session, que servem para trocar informações de uma pagina para outra.
 	session_start();
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Contador</title>
-        <link rel="stylesheet" type="text/css" href="css.css"/>
-    </head>
-    <body>
-    <?php
-    	// Percorrer todos os campos do array cadastro, que foi preenchido na páǵina cadastro.php 
-		foreach ($_SESSION["cadastro"] as $campo) {
-    		echo $campo;
+        foreach ($_SESSION['cadastro'] as $key => $value) {
+    		echo $key .':' . $value, '<br>';
 		}
     ?>
-    </body>
-</html>
